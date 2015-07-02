c4f3c6682c57e09a9af8d057eadb329baee59436
Egor Fedorov <egor.fedorov@emlid.com>
2015-07-02 17:52:17 +0000

    forms respond to submit

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask.ext.wtf import Form
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class WifiForm(Form):
     ssid = StringField("SSID", validators = [DataRequired()])
-    key = StringField("key", validators = [DataRequired()])
+    key = PasswordField("key", validators = [DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,10 +1,27 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from forms import WifiForm
+from os import system
 
 @app.route("/", methods = ["GET", "POST"])
 @app.route("/index", methods = ["GET", "POST"])
 def index():
     form = WifiForm()
     user = { "nickname" : "Reach" }
+
+    if form.validate_on_submit():
+        flash("New Wifi network data submitted: SSID == " + form.ssid.data + ", key == " + "*" * len(form.key.data) + ".")
+
+        # we got the SSID and key validated, that means we have to add 
+        # them to our boot script (/etc/rc.local) and to the wpa_supplicant file 
+        with open("/etc/rc.local", "w") as f:
+            data = f.readlines()
+
+#        data[39] = data[39][:-1] +  
+
+        cmd = ["wpa_passphrase", form.ssid.data, form.key.data, ">>", "/etc/wpa_supplicant/wpa_supplicant/conf"]
+        print("Running " + cmd)
+        # system(" ".join(cmd))
+        return redirect("/")
+
     return render_template("index.html", title = "ReachSetup Home", user = user, form = form)
