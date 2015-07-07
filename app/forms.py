dc1a35ece97b6c0baf9b1de01568d722d7704a87
Egor Fedorov <egor.fedorov@emlid.com>
2015-07-07 16:26:10 +0000

    length validator added

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class WifiForm(Form):
-    ssid = StringField("SSID", validators = [DataRequired()])
-    key = PasswordField("key", validators = [DataRequired()])
+    ssid = StringField("SSID", validators = [DataRequired(), Length(min = 0, max = 32])
+    key = PasswordField("key", validators = [DataRequired(), Length(min = 8, max = 63])
