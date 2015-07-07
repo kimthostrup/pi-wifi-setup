27efb34399f801ac00eb32e1c83eea74d00218f2
Egor Fedorov <egor.fedorov@emlid.com>
2015-07-07 17:29:20 +0000

    reshuffle inside views. added wifi scanning function. ssid is now a radio button element

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, RadioField
 from wtforms.validators import DataRequired, Length
 
 class WifiForm(Form):
-    ssid = StringField("SSID", validators = [DataRequired(), Length(min = 0, max = 32])
-    key = PasswordField("key", validators = [DataRequired(), Length(min = 8, max = 63])
+    ssid = RadioField("SSID", coerce = int)
+    # ssid = StringField("SSID", validators = [DataRequired(), Length(min = 0, max = 32])
+    key = PasswordField("key", validators = [DataRequired(), Length(min = 8, max = 63)])
--- app/views.py
+++ app/views.py
@@ -1,41 +1,58 @@
 from flask import render_template, flash, redirect
 from app import app
 from forms import WifiForm
-from os import system
+from os import system, popen
+
+def scanAvailableNetworks():
+    cmd = ["iwlist", "wlan0", "scan", "|", "grep", "ESSID"]
+    cmd = " ".join(cmd)
+
+    proc = popen(cmd)
+    output = proc.read()
+    print("### NETWORK SCAN OUTPUT ###")
+    print(output)
+    proc.close()
+
+def addNewNetwork(ssid, key):
+
+    # we got SSID and key validated, now we should add them
+    # to our startup script
+
+    with open("/etc/rc.local", "r") as f:
+        data = f.readlines()
+
+    # now we will be looking for the added network during startup
+    data[39] = data[39][:-2] + "'" + str(form.ssid.data) + "' )\n"
+
+    with open("/etc/rc.local", "w") as f:
+        f.writelines(data)
+
+    # we have the key, and so should the wpa_supplicant
+    cmd = ["wpa_passphrase", str(form.ssid.data), str(form.key.data), ">>", "/etc/wpa_supplicant/wpa_supplicant.conf"]
+    cmd = " ".join(cmd)
+    system(cmd)
 
 @app.route("/", methods = ["GET", "POST"])
 @app.route("/index", methods = ["GET", "POST"])
 def index():
     form = WifiForm()
-    user = { "nickname" : "Reach" }
+
+    form.ssid.choices = [(1, "LOL"), (2, "ROFL")]
+
+    scanAvailableNetworks()
 
     if form.validate_on_submit():
         flash("New Wifi network data submitted: SSID == " + form.ssid.data + ", key == " + "*" * len(form.key.data) + ".")
         print("New Wifi network data submitted: SSID == " + str(form.ssid.data) + ", key == " + str(form.key.data) + ".")
 
-        # we got the SSID and key validated, that means we have to add 
-        # them to our boot script (/etc/rc.local) and to the wpa_supplicant file 
-        with open("/etc/rc.local", "r") as f:
-            data = f.readlines()
+        addNewNetwork(str(form.ssid.data), str(form.key.data))
 
-        print(str(data[39]))
-
-        data[39] = data[39][:-2] + "'" + str(form.ssid.data) + "' )\n"
-
-        print("Hello! this is the new /etc/rc.local file")
-        print(str(data[39]))
-
-        with open("/etc/rc.local", "w") as f:
-             f.writelines(data)
-
-        cmd = ["wpa_passphrase", str(form.ssid.data), str(form.key.data), ">>", "/etc/wpa_supplicant/wpa_supplicant.conf"]
-        cmd = " ".join(cmd)
-        print("Running " + cmd)
-        system(cmd)
-
+        # to apply changes and try to connect to the new network we have to reboot
         cmd = "reboot"
         system(cmd)
 
         return redirect("/")
 
     return render_template("index.html", title = "ReachSetup Home", form = form)
+
+
